4ab314dc37a71944c24349e05bbaae376d533d4a
ikhsanalatsary <ikhsan.alatsary@yahoo.com>
2017-05-01 17:48:32 +0700

    :cool: (refactor) code, rule eslint

--- server/controller/contacts.controller.js
+++ server/controller/contacts.controller.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import Contact from '../model/contacts.model';
 
 exports.create = (req, res) => {
@@ -6,15 +7,15 @@ exports.create = (req, res) => {
 
   let bodyTitle;
   let bodyCompany;
-  let bodyName = JSON.parse(body.name);
-  let bodyEmail = JSON.parse(body.email);
-  let bodyPhone = JSON.parse(body.phone);
-  let bodyAddress = JSON.parse(body.address);
+  const bodyName = JSON.parse(body.name);
+  const bodyEmail = JSON.parse(body.email);
+  const bodyPhone = JSON.parse(body.phone);
+  const bodyAddress = JSON.parse(body.address);
   if (typeof body.title !== 'undefined') bodyTitle = JSON.parse(body.title);
   if (typeof body.company !== 'undefined') bodyCompany = JSON.parse(body.company);
 
-  var { avatar } = body;
-  let person = {
+  const { avatar } = body;
+  const person = {
     name: bodyName,
     title: bodyTitle,
     email: bodyEmail,
@@ -24,72 +25,71 @@ exports.create = (req, res) => {
     avatar,
   };
 
-  var contact = new Contact(person);
+  const contact = new Contact(person);
 
-  contact.save(err => {
+  contact.save((err) => {
     if (err) return handleError(res, err);
     return res.sendStatus(200);
   });
-
 };
 
-exports.show = (req, res, next) => {
-  var contactId = req.params.id;
-  Contact.findById(contactId, function (err, contact) {
+exports.show = (req, res) => {
+  const contactId = req.params.id;
+  Contact.findById(contactId, (err, contact) => {
     if (err) return res.status(400).json(err);
-    res.status(200).json(contact);
+    return res.status(200).json(contact);
   });
 };
 
-exports.index = (req, res, next) => {
-  let query = {};
+exports.index = (req, res) => {
+  const query = {};
   if (req.query.favorite) query.favorite = Boolean(req.query.favorite);
 
-  Contact.find(query, null, { sort: { name: 1 } }, function (err, contacts) {
+  Contact.find(query, null, { sort: { name: 1 } }, (err, contacts) => {
     if (err) return res.sendStatus(404);
-    if (contacts.length == 0) return res.sendStatus(404);
+    if (contacts.length === 0) return res.sendStatus(404);
     return res.status(200).json(contacts);
   });
 };
 
-exports.update = (req, res, next) => {
-  let body = req.body;
-  Contact.findById(req.params.id, function (err, contact) {
+exports.update = (req, res) => {
+  const body = req.body;
+  Contact.findById(req.params.id, (err, contact) => {
     if (req.file) {
       contact.avatar = req.file.filename;
     }
 
-    var bodyTitle;
-    var bodyCompany;
     contact.name = JSON.parse(body.name);
     contact.email = JSON.parse(body.email);
     contact.phone = JSON.parse(body.phone);
     contact.address = JSON.parse(body.address);
+    let bodyTitle;
+    let bodyCompany;
     if (typeof body.title !== 'undefined') bodyTitle = JSON.parse(body.title);
     if (typeof body.company !== 'undefined') bodyCompany = JSON.parse(body.company);
 
     contact.save((err, result) => {
       if (err) return res.status(400).json(err);
-      res.status(200).json(result);
+      return res.status(200).json(result);
     });
   });
 };
 
-exports.delete = (req, res, next) => {
-  Contact.remove({ _id: req.params.id }, function (err) {
+exports.delete = (req, res) => {
+  Contact.remove({ _id: req.params.id }, (err) => {
     if (err) return res.sendStatus(400);
-    res.sendStatus(200);
+    return res.sendStatus(200);
   });
 };
 
-exports.patch = (req, res, next) => {
-  Contact.findById(req.params.id, function (err, contact) {
+exports.patch = (req, res) => {
+  Contact.findById(req.params.id, (err, contact) => {
     contact.favorite = req.body.favorite;
     if (req.body.name) contact.name = req.body.name;
 
     contact.save((err, result) => {
       if (err) return res.json(400, err);
-      res.status(200).json(result);
+      return res.status(200).json(result);
     });
   });
 };
